eaeef9b0b7eb7350276dfec653aa9ce44cba771d
Darrian Soto <ddog350@gmail.com>
2019-10-01 18:34:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.map {|name| }
 end
 
 def find_the_cheese# code an argument here
